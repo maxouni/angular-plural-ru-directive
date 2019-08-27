0aab51e797a3b23242edadb11888b9e78825212b
Max Mishin <max@mishin.io>
2019-08-27 15:56:18 +0300

    fix code style bug

--- plural-ru.directive.ts
+++ plural-ru.directive.ts
@@ -1,19 +1,26 @@
 import {Directive, ElementRef, Input, OnChanges} from '@angular/core';
 
 @Directive({
-  selector: '[appPluralRu]'
+    selector: '[appPluralRu]'
 })
-export class PluralRuDirective implements OnChanges{
+export class PluralRuDirective implements OnChanges {
 
-  @Input() arVariants: string[] =  [];
-  @Input() value: number;
+    @Input() arVariants: string[] = [];
+    @Input() value: number;
 
-  constructor(
-    private el: ElementRef
-  ) { }
+    constructor(
+        private el: ElementRef
+    ) {
+    }
 
-  ngOnChanges(): void {
-    this.el.nativeElement.innerHTML =
-      this.value%10==1&&this.value%100!=11?this.arVariants[0]:(this.value%10>=2&&this.value%10<=4&&(this.value%100<10||this.value%100>=20)?this.arVariants[1]:this.arVariants[2]);
-  }
+    ngOnChanges(): void {
+        this.el.nativeElement.innerHTML =
+            this.value % 10 === 1 && this.value % 100 !== 11
+                ? this.arVariants[0]
+                : this.value % 10 >= 2 &&
+                this.value % 10 <= 4 &&
+                (this.value % 100 < 10 || this.value % 100 >= 20)
+                ? this.arVariants[1]
+                : this.arVariants[2];
+    }
 }
